a70199159e3c3e7e1679b936ee1ef9b780e699c3
garimamangalhara <33093141+garimamangalhara@users.noreply.github.com>
2019-07-13 20:22:31 +0530

    Update index.js

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ var app = require('express')();
 var http = require('http').createServer(app);
 var io = require('socket.io')(http);
 const fs = require('fs');
-const buttonPressesLogFile = './logger.txt';
+const logFile = './logger.txt';
 
 // app.get('/', function(req, res){
 //   res.send('<h1>Hello world</h1>');
@@ -11,24 +11,14 @@ app.get('/', function (req, res) {
     res.sendFile(__dirname + '/index.html');
 });
 io.on('connection', function (socket) {
-    socket.on('chat message', function (msg) {
-        io.emit('chat message', msg);
-    });
-});
-
-fs.watch(buttonPressesLogFile, (event, filename) => {
-    if (filename) {
-        console.log(`${filename} file Changed`);
         var lastTen;
-        fs.readFile(buttonPressesLogFile, 'utf-8', (err, data) => {
+        fs.readFile(logFile, 'utf-8', (err, data) => {
             if (err) {
                 console.error(err)
                 return
             }
-            console.log(data)
             const lines = data.split('\n')
             if (lines.length <= 10) {
-                console.log(lines)
             }
             else {
                 lastTen = lines.slice(-10).join('\n')
@@ -37,23 +27,28 @@ fs.watch(buttonPressesLogFile, (event, filename) => {
             io.emit('chat message', lastTen);
 
         })
+});
+
+fs.watch(logFile, (event, filename) => {
+    if (filename) {
+        console.log(`${filename} file Changed`);
+        var lastTen;
+        fs.readFile(logFile, 'utf-8', (err, data) => {
+            if (err) {
+                console.error(err)
+                return
+            }
+            const lines = data.split('\n')
+            if (lines.length <= 10) {
+            }
+            else {
+                lastTen = lines.slice(-10).join('\n')
+            }
+            io.emit('chat message', lastTen);
+
+        })
     }
 });
-fs.readFile(buttonPressesLogFile, 'utf-8', (err, data) => {
-    if (err) {
-        console.error(err)
-        return
-    }
-    console.log(data)
-    const lines = data.split('\n')
-    if (lines.length <= 10) {
-        console.log(lines)
-    }
-    else {
-        let lastTen = lines.slice(-10)
-        console.log("last ten:", lastTen)
-    }
-})
 http.listen(3000, function () {
     console.log('listening on *:3000');
 });
